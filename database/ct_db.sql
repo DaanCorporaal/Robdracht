5793a9cc8a7619123e3c807c781a1e35afe4fb78
Noah Lammertsma <noah5073@gmail.com>
2018-06-01 11:53:10 +0100

    database v3

--- database/ct_db.sql
+++ database/ct_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 01, 2018 at 12:39 PM
+-- Generation Time: Jun 01, 2018 at 12:52 PM
 -- Server version: 10.1.25-MariaDB
 -- PHP Version: 5.6.31
 
@@ -118,6 +118,8 @@ CREATE TABLE `personal_details` (
   `email` varchar(100) NOT NULL,
   `password` varchar(128) NOT NULL,
   `salt` varchar(256) NOT NULL,
+  `confirmed` tinyint(1) NOT NULL,
+  `confirm_code` int(11) NOT NULL,
   `classname` varchar(20) NOT NULL,
   `school_class` tinyint(1) NOT NULL,
   `bpv_name` varchar(100) NOT NULL,
