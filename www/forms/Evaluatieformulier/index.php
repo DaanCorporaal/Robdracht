32dcb93ca30d03d811ec54710ee4e48bc59970c8
shihaab <roushi02@gmail.com>
2018-06-19 12:58:58 +0200

    form update

--- www/forms/Evaluatieformulier/index.php
+++ www/forms/Evaluatieformulier/index.php
@@ -1,48 +1,41 @@
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-    <!-- Datum voorbereiding label-->
-    <label for="datum">
-        Datum voorbereiding:
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" class="mainForm">
+    <div class="wrapper">
+        <h2>Lesvoorbereiding form</h2>
+        <!-- Datum voorbereiding label-->
+        <label for="datum">Datum voorbereiding</label><br>
         <input type="date" id="datum" name="datum" value="<?php echo $datum;?>">
         <!-- Datum voorbereiding error-->
         <span class="error">* <?php echo $datumErr;?></span>
-    </label>
-    <br><br>
-    <!-- Evaluatie van label-->
-    <label for="naam">
-        Evaluatie van... :
+        <br><br>
+        <!-- Evaluatie van label-->
+        <label for="naam">Evaluatie van...</label><br>
         <input type="text" name="naam" id="naam" value="<?php echo $naam;?>">
         <!-- Schoolvak error-->
         <span class="error">* <?php echo $naamErr;?></span>
-    </label>
-    <br><br>
-    <!-- Ik ben tevreden-->
-    <label for="tevreden">
-        Ik ben tevreden:
-        <input type="radio" name="tevreden" <?php if (isset($tevreden) && $tevreden=="2") echo "checked";?> value="2">Ontevreden
+        <br><br>
+        <!-- Ik ben tevreden-->
+        <label for="tevreden">Ik ben tevreden</label><br><br><span class="radio-a">ontevreden</span><span class="radio-b">Tevrede</span><br>
+        <input type="radio" name="tevreden" <?php if (isset($tevreden) && $tevreden=="2") echo "checked";?> value="2">
         <input type="radio" name="tevreden" <?php if (isset($tevreden) && $tevreden=="4") echo "checked";?> value="4">
         <input type="radio" name="tevreden" <?php if (isset($tevreden) && $tevreden=="6") echo "checked";?> value="6">
         <input type="radio" name="tevreden" <?php if (isset($tevreden) && $tevreden=="8") echo "checked";?> value="8">
-        <input type="radio" name="tevreden" <?php if (isset($tevreden) && $tevreden=="10") echo "checked";?> value="10">Tevrede
+        <input type="radio" name="tevreden" <?php if (isset($tevreden) && $tevreden=="10") echo "checked";?> value="10">
         <!-- Evaluatie van error-->
         <span class="error">* <?php echo $tevredenErr;?></span>
-    </label>
-    <br><br>
-    <!-- wat ging er goed?-->
-    <label for="goed">
-        Wat ging er goed?:
+        <br><br>
+        <!-- wat ging er goed?-->
+        <label for="goed">Wat ging er goed?</label><br>
         <input type="text" id="goed" name="goed" value="<?php echo $goed;?>">
         <!-- Wat ging er goed? error-->
         <span class="error">* <?php echo $goedErr;?></span>
-    </label>
-    <br><br>
-    <!-- Wat kon er beter?-->
-    <label for="beter">
-        Wat kon beter?:
+        <br><br>
+        <!-- Wat kon er beter?-->
+        <label for="beter">Wat kon beter?</label><br>
         <input type="text" id="beter" name="beter" value="<?php echo $beter;?>">
         <!-- Wat kon er beter? error-->
         <span class="error">* <?php echo $beterErr;?></span>
-    </label>
-    <br><br>
+        <br><br>
 
-    <input type="submit" name="submit" value="Submit">
+        <input type="submit" name="submit" value="Opslaan" class="confirm">
+    </div>
 </form>
